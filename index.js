739de21f42d2082dbb573307826553571f9be6d8
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-05 17:17:16 +0300

    feat: create simple server

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import config from 'dotenv/config'
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}...`));
