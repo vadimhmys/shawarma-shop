3694680e81df27d69e8a2e88da98cdee7ee80724
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-06 18:32:01 +0300

    feat: create start function

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import config from 'dotenv/config'
+import config from 'dotenv/config';
+import sequelize from './sequelize';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}...`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}...`));
+  } catch (err) {}
+};
+
+start();
---
+++ sequelize.js
@@ -0,0 +1,26 @@
+import {Sequelize} from 'sequelize'
+
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
+  })
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST
+    }
+  );
+}
+
+export default sequelize;
