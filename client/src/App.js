f7bd49a893756cd05a5ff500e01e6feb3e108c5b
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-10 17:13:41 +0300

    feat: add Categories

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Card from './components/Card';
+import Categories from './components/Categories';
 import Header from './components/Header';
 import { data } from './data';
 
@@ -9,6 +10,7 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
+            <Categories/>
           </div>
           <h2 className="content__title">Все шавухи</h2>
           <div className="content__items">
---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Новинки</li>
+        <li>Курица</li>
+        <li>Говядина</li>
+        <li>Баранина</li>
+        <li>Рыба</li>
+        <li>Грибы</li>
+        <li>Острые</li>
+        <li>Вегетарианские</li>
+        <li>Сыр</li>
+        <li>Без лука</li>
+      </ul>
+    </div>
+  );
+}
