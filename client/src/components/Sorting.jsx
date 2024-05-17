022ad4d331304251476a24c5ba70b2a53089c1d7
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-17 22:39:48 +0300

    feat(sorting): implement the logic for displaying the popup and its elements

--- client/src/components/Sorting.jsx
+++ client/src/components/Sorting.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { MdOutlineArrowDropDownCircle } from 'react-icons/md';
+import React, { useState } from 'react';
+import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from 'react-icons/md';
 
 export default function Sorting() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const signs = ['произвольно', 'цене', 'алфавиту'];
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+    setIsVisible(false);
+  };
+
   return (
     <div className="sorting">
       <div className="sorting__top">
-        <MdOutlineArrowDropDownCircle
-          size="30"
-          style={{ color: '#323232', cursor: 'pointer', marginRight: '10' }}
-        />
-        <div className='sorting__top-title'>Сортировать по:</div>
+        {isVisible ? (
+          <MdOutlineArrowDropDown size="30" style={{ color: '#323232', marginRight: '10' }} />
+        ) : (
+          <MdOutlineArrowDropUp size="30" style={{ color: '#323232', marginRight: '10' }} />
+        )}
+
+        <div className="sorting__top-title">
+          Сортировать по: <span onClick={() => setIsVisible(!isVisible)}>{signs[activeIndex]}</span>
+        </div>
       </div>
-      <div className="sorting__popup">
-        <ul>
-          <li className="active">произвольно</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {isVisible && (
+        <div className="sorting__popup">
+          <ul>
+            {signs.map((s, i) => (
+              <li
+                key={s}
+                className={i === activeIndex ? 'active' : ''}
+                onClick={() => handleClick(i)}>
+                {s}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
