283d54ee0850d4209155b2ffb595d827708575dd
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-05 18:54:00 +0300

    feat: add simple pages Contacts and NotFound

---
+++ client/src/pages/Contacts/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/* import styles from './Contacts.module.scss'; */
+
+export default function Contacts() {
+  return <div>Contacts</div>;
+}
---
+++ client/src/pages/NotFound/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/* import styles from './NotFound.module.scss'; */
+
+export default function NotFound() {
+  return <div>NotFound</div>;
+}
