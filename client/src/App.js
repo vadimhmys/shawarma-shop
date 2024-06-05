cf18340a2271e10462bbfa2dca4a7c4d98e28028
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-05 19:08:36 +0300

    feat: create simple routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Main from './pages/Main';
+import Contacts from './pages/Contacts';
+import NotFound from './pages/NotFound';
 
 import styles from './App.module.scss';
 
@@ -10,7 +13,11 @@ function App() {
     <div className={styles.root}>
       <div className={styles.container}>
         <Header />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
