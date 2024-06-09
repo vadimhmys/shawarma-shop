b907f00ad330af18155d1d44d3c5a798dbcdd40c
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-09 17:28:44 +0300

    refactor(counter): highlight the sum

--- client/src/components/Counter/index.jsx
+++ client/src/components/Counter/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import styles from './Counter.module.scss';
 
-export default function Counter({price, maxCount}) {
+export default function Counter({ price, maxCount }) {
   const [multiplier, setMultiplier] = React.useState(0);
 
   const incrementCounter = (e) => {
     e.preventDefault();
     if (multiplier === maxCount) return;
     setMultiplier(multiplier + 1);
-  }
+  };
 
   const decrementCounter = (e) => {
     e.preventDefault();
     if (multiplier === 0) return;
     setMultiplier(multiplier - 1);
-  }
+  };
 
   let formatter = new Intl.NumberFormat('ru', {
     minimumFractionDigits: 2,
@@ -23,15 +23,19 @@ export default function Counter({price, maxCount}) {
 
   return (
     <div className={styles.root}>
-      <p className={styles.sum}>{formatter.format(multiplier * price)} р.</p>
+      <p className={multiplier === 0 ? styles.sum : styles.sum + ' ' + styles.dedicated}>{multiplier === 0 ? 0 : formatter.format(multiplier * price)} р.</p>
       <div className={styles.counter}>
-        <button className={styles.button} onClick={decrementCounter}>&#8722;</button>
+        <button className={styles.button} onClick={decrementCounter}>
+          &#8722;
+        </button>
         <div className={styles.quantity}>
           <span>{multiplier}</span>
           <span>x</span>
           <span>{formatter.format(price)} р</span>
         </div>
-        <button className={styles.button} onClick={incrementCounter}>&#43;</button>
+        <button className={styles.button} onClick={incrementCounter}>
+          &#43;
+        </button>
       </div>
     </div>
   );
