2f7c81d4af08102e305f41e68364020cdf3e9b66
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-02 21:20:27 +0300

    feat(search): focus on the input field when clearing it

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -6,18 +6,26 @@ import { SearchContext } from '../../App';
 import styles from './Search.module.scss';
 
 export default function Search() {
+  const inputRef = React.useRef(null);
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+
+  const handleClearInput = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
   return (
     <div className={styles.root}>
       <CiSearch className={styles.search__icon} />
       <input
+        ref={inputRef}
         className={styles.input}
         value={searchValue}
         placeholder="Поиск шаурмы..."
         onChange={(e) => setSearchValue(e.target.value)}
       />
       {searchValue && (
-        <IoCloseOutline className={styles.close__icon} onClick={() => setSearchValue('')} />
+        <IoCloseOutline className={styles.close__icon} onClick={handleClearInput} />
       )}
     </div>
   );
