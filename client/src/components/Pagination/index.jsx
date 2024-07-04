7bade48e03628c543dc5a4a3f085c424748c5cf7
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-05 00:34:35 +0300

    refactor(main): improve improve request for getting shawarmas

--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -4,9 +4,6 @@ import ReactPaginate from 'react-paginate';
 import styles from './Pagination.module.scss';
 
 export default function Pagination({pageCount, onPageChange, currentPage}) {
-  console.log('forcePage: currentPage - 1 : ', currentPage - 1);
-  console.log('pagecount: ', pageCount);
-
   return (
     <div className={styles.root}>
       <ReactPaginate
--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -16,7 +16,6 @@ import styles from './Main.module.scss';
 export default function Main() {
   const dispatch = useDispatch();
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
-  const sortType = sort.sortCritery;
   const { searchValue } = React.useContext(SearchContext);
   const [shawarmas, setShawarmas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
@@ -46,22 +45,27 @@ export default function Main() {
     dispatch(setCurrentPage(number));
   };
 
-  React.useEffect(() => {
+  const fetchShawarmas = React.useCallback(() => {
+    const category = categoryId > 0 ? `categoryId=${categoryId}` : '';
+    const sortBy = sort.sortCritery.replace('-', '');
+    const order = sort.sortCritery.includes('-') ? 'DESC' : 'ASC';
+
     setIsLoading(true);
     axios
       .get(
-        `http://localhost:7000/api/shawarmas/getall?${
-          categoryId > 0 ? `categoryId=${categoryId}` : ''
-        }&sortBy=${sortType.replace('-', '')}&order=${
-          sortType.includes('-') ? 'DESC' : 'ASC'
-        }&search=${searchValue}&limit=${limit}&page=${currentPage}`,
+        `http://localhost:7000/api/shawarmas/getall?${category}&sortBy=${sortBy}&order=${order}&search=${searchValue}&limit=${limit}&page=${currentPage}`,
       )
       .then((res) => {
         setShawarmas(res.data.rows);
         setNumberOfItems(res.data.count);
       })
       .finally(() => setIsLoading(false));
-  }, [categoryId, sortType, searchValue, currentPage, limit]);
+  }, [categoryId, sort.sortCritery, searchValue, currentPage, limit]);
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    fetchShawarmas();
+  }, [categoryId, sort.sortCritery, searchValue, currentPage, limit, fetchShawarmas]);
 
   const skeletons = [...new Array(6)].map((_, i) => <CardLoader key={i} />);
   const items = shawarmas.map(
@@ -82,11 +86,7 @@ export default function Main() {
           hideModalWindow={hideModalWindow}
         />
       )}
-      <Pagination
-        pageCount={pageCount}
-        onPageChange={onPageChange}
-        currentPage={currentPage}
-      />
+      <Pagination pageCount={pageCount} onPageChange={onPageChange} currentPage={currentPage} />
     </div>
   );
 }
