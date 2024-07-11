7f4ea2244ede542126449e906b558b839b7090d4
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-11 11:30:52 +0300

    feat(basket): add additional info and buttons to basket page

--- client/src/pages/Basket/index.jsx
+++ client/src/pages/Basket/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { formatPrice } from '../../utils/formatPrice';
 import BasketItem from './BasketItem';
+import Button from '../../components/Button';
 
 import styles from './Basket.module.scss';
 
 export default function Basket() {
-  const {items} = useSelector(state => state.basket);
+  const items = useSelector(state => state.basket.items);
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+  const totalPrice = formatPrice(items.reduce((sum, item) => sum + item.count * parseFloat(item.price.replace(',','.')), 0));
   return (
     <div className={styles.root}>
       <h2 className={styles.title}>Корзина</h2>
+      <button className={styles.clearBtn}>Очистить корзину</button>
       <ul className={styles.list}>
         {
           items.map(item => <BasketItem key={item.id + item.cake + item.weight + JSON.stringify(item.addedComponentsList)} {...item}/>)
         }
       </ul>
+      <div className={styles.bottom}>
+        <p className={styles.text}>Общее количество шавух: <span>{totalCount}</span> на сумму <span>{totalPrice}</span> руб.</p>
+        <Button>Оформить заказ</Button>
+      </div>
     </div>
   );
 }
