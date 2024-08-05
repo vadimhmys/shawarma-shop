b9d51f2d11ed0bcbb6ceb2c38df85c8ec79e6510
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-05 19:29:12 +0300

    feat(FormField): create FormField component

--- client/src/ui-kit/index.ts
+++ client/src/ui-kit/index.ts
@@ -2,10 +2,12 @@ import Input from './Input';
 import Switcher from './Switcher';
 import Counter from './Counter';
 import Button from './Button';
+import FormField from './FormField';
 
 export {
   Input,
   Switcher,
   Counter,
-  Button
+  Button,
+  FormField
 }
