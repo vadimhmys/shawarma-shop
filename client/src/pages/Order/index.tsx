31e793282f0c3d1b7dfcc8f3cce36caa31e0230b
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-11 20:27:43 +0300

    feat(order): add loader

--- client/src/pages/Order/index.tsx
+++ client/src/pages/Order/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 import PageTitle from '../../components/PageTitle';
 import OrderForm from './OrderForm';
 import { useSelector } from 'react-redux';
 import { selectOrderStatus } from '../../redux/order/selectors';
 import { StatusEnum } from '../../redux/order/types';
 import OrderError from './OrderError';
+import styles from './Order.module.scss';
 
 const Order: React.FC = () => {
   const status = useSelector(selectOrderStatus);
 
   if (status === StatusEnum.LOADING) {
-    return <div>LOADING</div>;
+    return (
+      <div className={styles.loaderWrapper}>
+        <ReactLoading type={'spin'} color={'red'} height={80} width={80} />
+      </div>
+    );
   }
 
   if (status === StatusEnum.ERROR) {
