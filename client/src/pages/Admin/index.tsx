97eb657aec914eb7f0dc3c5107df51677f3a1fa5
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-16 23:24:54 +0300

    feat(Admin): create simple layout for category block

--- client/src/pages/Admin/index.tsx
+++ client/src/pages/Admin/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import styles from './Admin.module.scss';
+import clsx from 'clsx';
+import PageTitle from '../../components/PageTitle';
+import { Button } from '../../ui-kit';
+
 const Admin: React.FC = () => {
-  return <h1>Панель управления</h1>;
+  return (
+    <div className={styles.root}>
+      <PageTitle>Панель управления</PageTitle>
+      <div className={clsx(`${styles.infoBlock}`, `${styles.category}`)}>
+        <h3 className={styles.infoBlock__title}>Категории</h3>
+        <div className={styles.infoBlock__content}>
+          <Button>Список категорий</Button>
+          <Button>Новая категория</Button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Admin;
