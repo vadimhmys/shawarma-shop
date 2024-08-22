e91b35e99929a02f39458d001d0e511fd88ae97e
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-22 19:02:30 +0300

    refactor: open middleware for shawarma routers

--- routes/shawarma.js
+++ routes/shawarma.js
@@ -7,11 +7,11 @@ const router = new express.Router();
 
 router.get('/getall', ShawarmaController.getAll);
 router.get('/getone/:id([0-9]+)', ShawarmaController.getOne);
-router.post('/create', /*authMiddleware, adminMiddleware,*/ ShawarmaController.create);
-router.put('/update/:id([0-9]+)', /*authMiddleware, adminMiddleware,*/ ShawarmaController.update);
+router.post('/create', authMiddleware, adminMiddleware, ShawarmaController.create);
+router.put('/update/:id([0-9]+)', authMiddleware, adminMiddleware, ShawarmaController.update);
 router.delete(
   '/delete/:id([0-9]+)',
-  /*authMiddleware, adminMiddleware,*/ ShawarmaController.delete,
+  authMiddleware, adminMiddleware, ShawarmaController.delete,
 );
 
 export default router;
