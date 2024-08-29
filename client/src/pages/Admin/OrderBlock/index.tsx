909d772f9312ae59815fd8128ff1cbaba5ee03cc
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-29 22:20:32 +0300

    feat(OrderBlock): implement fetching orders

--- client/src/pages/Admin/OrderBlock/index.tsx
+++ client/src/pages/Admin/OrderBlock/index.tsx
@@ -2,9 +2,33 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from '../Admin.module.scss';
 import { Button } from '../../../ui-kit';
+import { adminGetAll } from '../../../http/orderAPI';
+
+type OrderForAdminType = {
+  prettyCreatedAt: string;
+  prettyUpdatedAt: string;
+  id: number;
+  userName: string;
+  phone: string;
+  waitingTime: number;
+  comment: string;
+  payment: 'CASH' | 'CARD';
+  amount: string;
+  status: number;
+  createdAt: string;
+  updatedAt: string;
+  userId: number;
+};
 
 const OrderBlock: React.FC = () => {
   const [isShowOrderList, setIsShowOrderList] = React.useState(false);
+  const [orders, setOrders] = React.useState<OrderForAdminType[]>([]);
+
+  React.useEffect(() => {
+    adminGetAll()
+      .then((data) => setOrders(data))
+      .catch(() => alert('Не удалось загрузить заказы'));
+  }, []);
   return (
     <div className={clsx(`${styles.infoBlock}`, `${styles.order}`)}>
       <h3 className={styles.infoBlock__title}>Заказы</h3>
@@ -13,7 +37,13 @@ const OrderBlock: React.FC = () => {
           {isShowOrderList ? 'Скрыть' : 'Показать'} список
         </Button>
         {isShowOrderList && (
-          <span>Orders list</span>
+          <ul>
+            {orders.map((order) => (
+              <li key={order.id}>
+                {order.userName}
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </div>
